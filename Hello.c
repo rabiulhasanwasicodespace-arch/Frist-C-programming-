976fdad36d99c43faa54e5d18b9b53fe2cfb7964
Rabiul Hasan Wasi <288028160+rabiulhasanwasicodespace-arch@users.noreply.github.com>
2026-07-06 00:13:55 +0600

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=12,b=34,c=54,d=21,sum,sub;
+    float e=2.3,f=3.4,g,h,mul;
+    sum=a+b;
+    sub=c-d;
+    mul=e*f;
+    scanf("%f%f",&g,&h);
+    printf("%d%d",g,h);
+    printf("Sum:%d\n",sum);
+    printf("Subtraction:%d\n",sub);
+    printf("Multipication:%f\n",mul);
+    printf("Hello\n");
+    return 0;
+}
